62a9a61fadb494c15d7b2b1101be9f97b299216c
Adam Primer <adamprimersrl@gmail.com>
2015-04-25 06:24:19 +1000

    Added origin to Vector2, creating Vector4 from Vector2 and w/h

--- Vector.cs
+++ Vector.cs
@@ -1,14 +1,28 @@
 ﻿namespace LiveSplit.OriAndTheBlindForest
 {
+    enum Origin
+    {
+        Center,
+        BottomLeft
+    }
+
     public class Vector2
     {
         public float X { get; set; }
         public float Y { get; set; }
+        public Origin origin { get; set; }
 
         public Vector2(float x, float y) {
             this.X = x;
             this.Y = y;
+            this.origin = Origin.BottomLeft;
         }
+        public Vector2(float x, float y, Origin origin) {
+            this.X = x;
+            this.Y = y;
+            this.origin = origin;
+        }
+
         public Vector2(string cordinates) {
             string[] cords = cordinates.Split(new char[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
             if (cords.Length == 2) {
@@ -84,6 +98,18 @@
                 float.TryParse(cords[3], out temp);
                 this.H = temp;
             }
+        }
+        public Vector4(Vector2 pos, float w, float h) {
+            if (pos.origin == Origin.Center) {
+                this.X = pos.X - w / 2;
+                this.Y = pos.Y + h / 2;
+            } else {
+                this.X = pos.X;
+                this.Y = pos.Y;
+            }
+
+            this.W = w;
+            this.H = h;
         }
 
         public override string ToString() {
