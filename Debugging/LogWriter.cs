f32ff1532afdfbf2d6d187700a00c08c3ed278d0
Adam Primer <adamprimersrl@gmail.com>
2015-05-04 21:21:18 +1000

    Added LogWriter.

---
+++ Debugging/LogWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace LiveSplit.OriAndTheBlindForest.Debugging
+{
+    class LogWriter
+    {
+        public static void WriteLine(string format, params object[] arg) {
+#if DEBUG
+            string str = format;
+            if (arg.Length > 0)
+                str = String.Format(format, arg);
+
+            StreamWriter wr = new StreamWriter("_oriauto.log", true);
+            wr.WriteLine("[" + DateTime.Now + "] " + str);
+            wr.Close();
+#endif
+        }
+    }
+}
